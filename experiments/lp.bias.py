123de43fb9c93cff90b906e4b150c134d17aeeb8
Peter Bloem <git@peterbloem.nl>
2020-07-28 19:16:31 +0200

    debug

--- experiments/lp.bias.py
+++ experiments/lp.bias.py
@@ -107,9 +107,9 @@ def go(arg):
     else:
         train, test = train, val
 
-    subjects   = list({s for s, _, _ in train})
-    predicates = list({p for _, p, _ in train})
-    objects    = list({o for _, _, o in train})
+    subjects   = torch.tensor(list({s for s, _, _ in train}), dtype=torch.long, device=d())
+    predicates = torch.tensor(list({p for _, p, _ in train}), dtype=torch.long, device=d())
+    objects    = torch.tensor(list({o for _, _, o in train}), dtype=torch.long, device=d())
     ccandidates = (subjects, predicates, objects)
 
     print(len(i2n), 'nodes')
@@ -173,34 +173,40 @@ def go(arg):
 
                     if ng > 0:
 
-                        tic()
-                        bs, _ = positives.size()
+                        with torch.no_grad():
+                            bs, _ = positives.size()
 
-                        cand = ccandidates[ctarget] if arg.limit_negatives else range(len(i2r if ctarget == 1 else i2n))
-                        corruptions = torch.tensor(random.choices(cand, k=bs * ng), dtype=torch.long, device=d()).view(bs, ng)
+                            tic()
+                            if arg.limit_negatives:
+                                cand = ccandidates[ctarget]
+                                mx = cand.size(0)
+                                idx = torch.empty(bs, ng, dtype=torch.long, device=d()).random_(0, mx)
+                                corruptions = cand[idx]
+                            else:
+                                mx = len(i2r) if ctarget == 1 else len(i2n)
+                                corruptions = torch.empty(bs, ng, dtype=torch.long, device=d()).random_(0, mx)
+                            tprep += toc()
 
-                        s, p, o = positives[:, 0:1], positives[:, 1:2], positives[:, 2:3]
-                        if ctarget == 0:
-                            s = torch.cat([s, corruptions], dim=1)
-                        if ctarget == 1:
-                            p = torch.cat([p, corruptions], dim=1)
-                        if ctarget == 2:
-                            o = torch.cat([o, corruptions], dim=1)
+                            s, p, o = positives[:, 0:1], positives[:, 1:2], positives[:, 2:3]
+                            if ctarget == 0:
+                                s = torch.cat([s, corruptions], dim=1)
+                            if ctarget == 1:
+                                p = torch.cat([p, corruptions], dim=1)
+                            if ctarget == 2:
+                                o = torch.cat([o, corruptions], dim=1)
 
-                        # -- NB: two of the index vectors s, p o are now size (bs, 1) and the other is (bs, ng+1)
-                        #    We will let the model broadcast these to give us a score tensor of (bs, ng+1)
-                        #    In most cases we can optimize the decoder to broadcast late for better speed.
+                            # -- NB: two of the index vectors s, p o are now size (bs, 1) and the other is (bs, ng+1)
+                            #    We will let the model broadcast these to give us a score tensor of (bs, ng+1)
+                            #    In most cases we can optimize the decoder to broadcast late for better speed.
 
-                        if arg.loss == 'bce':
-                            labels = torch.cat([torch.ones(bs, 1, device=d()), torch.zeros(bs, ng, device=d())], dim=1)
-                        elif arg.loss == 'ce':
-                            labels = torch.zeros(bs, dtype=torch.long, device=d())
-                            # -- CE loss treats the problem as a multiclass classification problem: for a positive triple,
-                            #    together with its k corruptions, identify which is the true triple. This is always triple 0.
-                            #    (It may seem like the model could easily cheat by always choosing triple 0, but the score
-                            #    function is order equivariant, so it can't choose by ordering.)
-
-                        tprep += toc()
+                            if arg.loss == 'bce':
+                                labels = torch.cat([torch.ones(bs, 1, device=d()), torch.zeros(bs, ng, device=d())], dim=1)
+                            elif arg.loss == 'ce':
+                                labels = torch.zeros(bs, dtype=torch.long, device=d())
+                                # -- CE loss treats the problem as a multiclass classification problem: for a positive triple,
+                                #    together with its k corruptions, identify which is the true triple. This is always triple 0.
+                                #    (It may seem like the model could easily cheat by always choosing triple 0, but the score
+                                #    function is order equivariant, so it can't choose by ordering.)
 
                         tic()
                         out = model(s, p, o)
@@ -245,11 +251,11 @@ def go(arg):
 
                 tbw.add_scalar('biases/train_loss', float(loss.item()), seen)
 
-            print(f'\n pred: forward {tforward:.4}, backward {tbackward:.4}')
-            print(f'    reg: forward {rforward:.4}, backward {rbackward:.4}')
-            print(f'            prep {tprep:.4}, loss {tloss:.4}')
-            print(f'  total: {toc():.4}')
-
+            if e == 0:
+                print(f'\n pred: forward {tforward:.4}, backward {tbackward:.4}')
+                print(f'    reg: forward {rforward:.4}, backward {rbackward:.4}')
+                print(f'            prep {tprep:.4}, loss {tloss:.4}')
+                print(f'  total: {toc():.4}')
 
             # Evaluate
             if ((e+1) % arg.eval_int == 0) or e == arg.epochs - 1:
